9e5e70d2dddeb38a4bca51d9fd8e8200578b2095
thuako <tjrgh822@gmail.com>
2021-12-10 08:54:46 +0000

    gemmini multi sim

--- soc-firesim/workload/make_ext2.sh
+++ soc-firesim/workload/make_ext2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python gen-benchmark-rootfs.py -w soc-gemmini.json -b ~/firesim/sw/firesim-software/images/br-base.img -s $softwaregemmini/build/overlay
+rm soc-gemmini-multisim/*.ext2
+python gen-benchmark-rootfs.py -w soc-gemmini-multisim.json -b ~/firesim/sw/firesim-software/images/br-base.img -s $softwaregemmini/build/overlay
